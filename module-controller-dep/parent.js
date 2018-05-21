b802b5386c965e0b287898213cef9e69f65b4d5c
TRICONHOME\sonam <narayanam.moukthik@mheducation.com>
2018-05-21 18:57:41 +0530

    oojs

--- module-controller-dep/parent.js
+++ module-controller-dep/parent.js
@@ -7,10 +7,17 @@
     angular.module("ParentModule", ['Child1Module'])
         .controller('ParentController', ['$scope', 'parentDataService', function($scope, parentDataService) {
             $scope.data = { "name": "shekhar" };
+            $scope.names = ['raghu', 'shekhar'];
+            $scope.showAlert = function () {
+                return "shekharVal";
+            }
         }])
         .factory('parentDataService', function() {
             var parentData = [{ name: "raghu", company: "tricon" }, { name: "shekhar", company: "tricon" }]
             return parentData;
+        })
+        .filter('toupper', function(data) {
+            return data.toUpperCase();
         });
 
 })(window.angular);
